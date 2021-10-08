74d33fb498346ba257b4e378272ffa53e685b829
Sam K <hello@samk.dev>
2021-10-08 20:06:10 +0200

    added .env support - some cleanup 🧹

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
