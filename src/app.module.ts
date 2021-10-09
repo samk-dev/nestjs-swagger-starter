e70eefea74e2284203b5d13eb08da91e4c3ee957
Sam K <hello@samk.dev>
2021-10-09 11:59:47 +0200

    Swagger UI Support
    - added support for swagger ui
    - added initial custom styles to swagger ui
    - added support for serving static assets in public folder
    - added .env-example variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ProductsControllerV1 } from './products/products.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [ProductsControllerV1],
   providers: [],
 })
